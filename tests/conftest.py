9fdec57b3c73210b7f7286f9397faccd2655c4db
Emiliy Feldman <feldlime@yandex.ru>
2021-07-04 17:05:38 +0300

    added tests

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+# pylint: disable=redefined-outer-name
+
+import pytest
+from fastapi import FastAPI
+from starlette.testclient import TestClient
+
+from reports_service.api.app import create_app
+from reports_service.settings import ServiceConfig, get_config
+
+
+@pytest.fixture
+def service_config() -> ServiceConfig:
+    return get_config()
+
+
+@pytest.fixture
+def app(service_config: ServiceConfig) -> FastAPI:
+    app = create_app(service_config)
+    return app
+
+
+@pytest.fixture
+def client(app: FastAPI) -> TestClient:
+    return TestClient(app=app)
