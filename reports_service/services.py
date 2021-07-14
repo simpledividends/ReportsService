7f5e1c453c49abc4a64383bf7d679bcbee29a05e
Emiliy Feldman <feldlime@yandex.ru>
2021-07-14 11:00:06 +0300

    added services

--- reports_service/services.py
+++ reports_service/services.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 
 from reports_service.auth import AuthService
 from reports_service.db.service import DBService
+from reports_service.queue import QueueService
 from reports_service.settings import ServiceConfig
+from reports_service.storage import StorageService
 
 
 def get_auth_service(app: FastAPI) -> AuthService:
@@ -14,6 +16,18 @@ def get_db_service(app: FastAPI) -> DBService:
     return app.state.db_service
 
 
+def get_queue_service(app: FastAPI) -> QueueService:
+    return app.state.queue_service
+
+
+def get_storage_service(app: FastAPI) -> StorageService:
+    return app.state.storage_service
+
+
+def make_auth_service(_: ServiceConfig) -> AuthService:
+    return AuthService()
+
+
 def make_db_service(config: ServiceConfig) -> DBService:
     db_config = config.db_config.dict()
     pool_config = db_config.pop("db_pool_config")
@@ -21,3 +35,11 @@ def make_db_service(config: ServiceConfig) -> DBService:
     pool = create_pool(**pool_config)
     service = DBService(pool=pool, **db_config)
     return service
+
+
+def make_queue_service(_: ServiceConfig) -> QueueService:
+    return QueueService()
+
+
+def make_storage_service(_: ServiceConfig) -> StorageService:
+    return StorageService()
