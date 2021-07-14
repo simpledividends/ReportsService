128bfc39c9a99a28907998abe05d5824c058d5ba
Emiliy Feldman <feldlime@yandex.ru>
2021-07-15 00:14:15 +0300

    added tests utils

---
+++ tests/helpers.py
@@ -0,0 +1,19 @@
+import typing as tp
+
+from sqlalchemy import inspect, orm, text
+
+from reports_service.db.models import Base
+
+
+def assert_all_tables_are_empty(
+    db_session: orm.Session,
+    exclude: tp.Collection[Base] = (),
+) -> None:
+    inspector = inspect(db_session.get_bind())
+    tables_all = inspector.get_table_names()
+    exclude_names = [e.__tablename__ for e in exclude]
+    tables = set(tables_all) - (set(exclude_names) | {"alembic_version"})
+    for table in tables:
+        request = text(f"SELECT COUNT(*) FROM {table}")
+        count = db_session.execute(request).fetchone()[0]
+        assert count == 0
