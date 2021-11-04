fa2121c0a2b082e54ce3eb3dc3eaf48aa186a84e
Emiliy Feldman <feldlime@yandex.ru>
2021-11-04 21:22:27 +0300

    added promocodes table

---
+++ migrations/versions/20211104172003_add_promocodes_table.py
@@ -0,0 +1,35 @@
+"""add_promocodes_table
+
+Revision ID: 4363041b49d1
+Revises: 9986a3a7aea0
+Create Date: 2021-11-04 17:20:03.256663
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import INTEGER, UUID, VARCHAR, TIMESTAMP
+
+
+# revision identifiers, used by Alembic.
+revision = '4363041b49d1'
+down_revision = '9986a3a7aea0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "promocodes",
+        sa.Column("promocode", VARCHAR(16), primary_key=True),
+        sa.Column("user_id", UUID, nullable=True),
+        sa.Column("valid_from", TIMESTAMP, nullable=False),
+        sa.Column("valid_to", TIMESTAMP, nullable=False),
+        sa.Column("rest_usages", INTEGER, nullable=False),
+        sa.Column("discount", INTEGER, nullable=False),
+
+        sa.PrimaryKeyConstraint("promocode"),
+    )
+
+
+def downgrade():
+    op.drop_table("promocodes")
